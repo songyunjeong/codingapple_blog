5ee89c94d06e616c0a9425ed27b84097f513d113
songyunjeong <yungunaa@naver.com>
2023-04-11 18:59:00 +0900

    props를 이용한 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [글제목, 글제목변경] = useState([
+    '남자 코트 추천',
+    '강남 우동 맛집',
+    '파이썬 독학',
+  ]);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+
+  function changeTitle() {
+    let copy = [...글제목];
+    copy[0] = '여자 코트 추천';
+    글제목변경(copy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='black-nav'>
+        <h4>ReactBlog</h4>
+      </div>
+      <button
+        onClick={() => {
+          changeTitle();
+        }}
+      >
+        글 수정
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        가나다순 정렬
+      </button>
+      {글제목.map((data, i) => {
+        return (
+          <div className='list' key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+              }}
+            >
+              {data}
+            </h4>
+            <button
+              onClick={() => {
+                let copy = [...따봉];
+                copy[i] = copy[i] + 1;
+                따봉변경(copy);
+              }}
+            >
+              👍🏻 {따봉[i]}
+            </button>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      {modal ? <Modal 글제목={글제목} changeTitle={changeTitle} /> : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className='modal'>
+      <h4>{props.글제목[0]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button
+        onClick={() => {
+          props.changeTitle();
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
 }
